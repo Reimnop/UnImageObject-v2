060247404951317601c3362907299904f919da9e
UserTCQ <rayngu2005@gmail.com>
2021-03-29 00:19:52 +0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UnImageObject_v2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ImgBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image|*.png;*.jpg";
+            ofd.ShowDialog();
+
+            if (!string.IsNullOrEmpty(ofd.FileName))
+            {
+                ImgField.Text = ofd.FileName;
+            }
+        }
+
+        private void OutBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Prefab|*.lsp";
+            sfd.ShowDialog();
+
+            if (!string.IsNullOrEmpty(sfd.FileName))
+            {
+                OutField.Text = sfd.FileName;
+            }
+        }
+
+        private void ConvertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# UnImageObject-v2
+ highly optimized image to project arrhythmia utility
